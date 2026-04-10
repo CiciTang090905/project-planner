bec5cefd708a84fab14446e9646254f6f7811148
CiciTang090905 <wtang28@jh.edu>
2026-04-09 23:01:38 -0400

    planner-07: Fetch tasks by project using a database filter

--- src/routes/projects.$projectId.tsx
+++ src/routes/projects.$projectId.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useQuery } from "convex/react";
-import { api } from "../../../convex/_generated/api";
-import type { Id } from "../../../convex/_generated/dataModel";
+import { api } from "../../convex/_generated/api";
+import type { Id } from "../../convex/_generated/dataModel";
 import KanbanBoard from "@/components/kanban-board";
 
 export const Route = createFileRoute("/projects/$projectId")({
